270561753b9ab75f5ae0cd261bbdb186bd7f4a4b
Blazebrain <davidadegoke16@gmail.com>
2023-03-07 05:32:33 +0100

    chore: Codebase setup, folder architecture setup, controllers setup, models and utils setup

---
+++ pkg/controllers/book-controller.go
@@ -0,0 +1,13 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/Blazebrain/blaze-books/pkg/models"
+)
+
+var NewBook models.Book
+
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	newBoos:=
+}
